e64e0b49d61665dc6e7b42e6f9d6bfe224ffadd5
Ivanildo da Silva Júnior <ivanildojunior2319@gmail.com>
2024-04-27 22:39:47 -0300

    tamo indo

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -24,6 +24,8 @@ function Api({ pokemon }) {
 
     return (
         <React.Fragment>
+            <img src={pokemonData['sprites']['other']['home']['front_default']} 
+            className='pokemon_image' alt='pokemon' style={{width: '30%',height: 'auto',top: '25%', left: '21%'}}/>
             <h1 className="pokemon_data">
                 <span className="pokemon_number">{pokemonData.id}</span> - <span className="pokemon_name">{pokemonData.name}</span>
             </h1>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,19 +12,10 @@ function Home() {
         <div className="home">
 
             <img src={background} alt="background" />
-
-            <div className="content">
-
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png" 
-                className="pokemon_image"
-                alt="Pokemon"/>
-                
-            </div>
-
             <Form className="custom-form" inputClassName="custom-input" />
             <NextButton className="next-button"/>
             <PrevButton className="prev-button"/>
-            <Api pokemon='5'/>
+            <Api pokemon='1'/>
         </div>
     )
 }
