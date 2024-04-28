0e7d9884e459b284a2cb949c1729a4eef1321092
jau italo <jau.batista@dcx.ufpb.br>
2024-04-28 18:35:17 -0300

    mais dois buttons criados na parte de mais infor

--- src/pages/MoreInfo.jsx
+++ src/pages/MoreInfo.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { FaHeart, FaStar } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import Infos from "../assets/images/Infos.png";
 import "../styles/MoreInfo.css";
-
 function MoreInfo() {
+
     return (
-        <div>
-            <img src={Infos} alt="Infos"/>
+        <div className="moreinfo">
+            <img src={Infos} alt="Infos" style={{ width: '1500px', height: 'auto' }} />
+            
+
+            <Link to="/favorites" className="favoritebtn">
+                <button className="favorites-btn">Favoritos<FaStar /></button>
+            </Link>
+
+            <button className="add-favorites">Adicionar aos Favoritos<FaHeart /></button>
+
         </div>
-    )
+    );
 }
 
-export default MoreInfo
+export default MoreInfo;
